af677ca60736ce9faf2fb4af5a3ad642911323d1
murajo <zorinmurajo@gmail.com>
2022-11-28 23:09:19 +0900

    [update] Controller get path #1

--- src/main/java/com/example/greeting/GreetingController.java
+++ src/main/java/com/example/greeting/GreetingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GreetingController{
 	
-	@GetMapping("/greeting")
+	@GetMapping("/")
 	public String getGreeting() {
 		return "greeting/greeting";
 	}
